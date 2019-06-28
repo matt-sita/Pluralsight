dd8ab7cc2ac4cea4ec0ca2c2ae5303cc48943ea0
Matt Kaelber <Matt.Kaelber@sita.aero>
2019-06-28 16:34:06 -0400

    updated to start up to the point of retrieving data via http

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
+import { IProduct } from './products';
+import { ProductService } from './product.service';
+
+@Pipe({
+  name: 'convertToSpaces'
+})
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+
+export class ProductListComponent implements OnInit{
+  constructor(private productService: ProductService) {
+  }
+
+  pageTitle: string = 'Product List!';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value:string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: IProduct[];
+  products: IProduct[] = [];
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+}
